9e6dd9126b12f21addcf16b059b67ec8ee712005
Jason Peng <jasonpeng7@Jasons-MBP-9.lan>
2024-09-17 14:14:08 -0700

    updated html

--- website-modal.js
+++ website-modal.js
@@ -50,6 +50,7 @@ window.onclick = function(event) {
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        modalOverlay.style.display = "none";
     }
 };
 
